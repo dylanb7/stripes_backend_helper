17f423eb09e94438cd946bb30000055c62f86241
Dylan Baker <dylanbaker7@gmail.com>
2023-06-08 10:55:01 -0400

    added page layout overrides

--- lib/RepositoryBase/QuestionBase/question_repo_base.dart
+++ lib/RepositoryBase/QuestionBase/question_repo_base.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:stripes_backend_helper/QuestionModel/question.dart';
 import 'package:stripes_backend_helper/RepositoryBase/AuthBase/auth_user.dart';
 
@@ -6,7 +7,18 @@ abstract class QuestionRepo<T extends QuestionHome> {
 
   QuestionRepo({required this.authUser});
 
+  Map<String, List<PageLayout>> getLayouts() => {};
+
   T get questions;
+}
+
+@immutable
+class PageLayout {
+  final List<Question> questions;
+
+  final String header;
+
+  const PageLayout(this.questions, this.header);
 }
 
 abstract class QuestionHome {

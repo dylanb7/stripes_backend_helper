887d9c926c6a03214863857b0fed109d6ac00898
Dylan Baker <dylanbaker7@gmail.com>
2023-09-28 17:31:51 -0400

    added period to record path

--- lib/RepositoryBase/QuestionBase/question_repo_base.dart
+++ lib/RepositoryBase/QuestionBase/question_repo_base.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:stripes_backend_helper/QuestionModel/question.dart';
 import 'package:stripes_backend_helper/RepositoryBase/AuthBase/auth_user.dart';
 
@@ -15,7 +15,7 @@ abstract class QuestionRepo<T extends QuestionHome> {
 @immutable
 class RecordPath {
   final List<PageLayout> pages;
-  final Duration? period;
+  final Period? period;
   const RecordPath(this.pages, this.period);
 }
 
@@ -32,4 +32,67 @@ abstract class QuestionHome {
   Map<String, Question> all = {};
 
   Question fromID(String id) => all[id] ?? Question.empty();
+}
+
+enum Period {
+  day,
+  week,
+  month,
+  year;
+
+  DateTime getValue(DateTime entry) {
+    switch (this) {
+      case Period.day:
+        return DateTime(entry.year, entry.month, entry.day);
+      case Period.week:
+        final DateTime prev = previous(entry, DateTime.monday);
+        return DateTime(prev.year, prev.month, prev.day);
+      case Period.month:
+        return DateTime(entry.year, entry.month);
+      case Period.year:
+        return DateTime(entry.year);
+    }
+  }
+
+  DateTimeRange getRange(DateTime entry) {
+    final DateTime start = getValue(entry);
+    switch (this) {
+      case Period.day:
+        final DateTime end = start.add(const Duration(days: 1));
+        return DateTimeRange(start: start, end: end);
+      case Period.week:
+        final DateTime end = start.add(const Duration(days: 7));
+        return DateTimeRange(start: start, end: end);
+      case Period.month:
+        final DateTime end = start.add(Duration(days: daysInMonth(entry)));
+        return DateTimeRange(start: start, end: end);
+      case Period.year:
+        final DateTime end = start.add(const Duration(days: 365));
+        return DateTimeRange(start: start, end: end);
+    }
+  }
+
+  const Period();
+}
+
+int daysInMonth(DateTime date) {
+  DateTime firstOfNextMonth;
+  if (date.month == 12) {
+    firstOfNextMonth = DateTime(date.year + 1, 1, 1, 12);
+  } else {
+    firstOfNextMonth = DateTime(date.year, date.month + 1, 1, 12);
+  }
+  return firstOfNextMonth.subtract(const Duration(days: 1)).day;
+}
+
+DateTime previous(DateTime date, int day) {
+  if (day == date.weekday) {
+    return date;
+  } else {
+    return date.subtract(
+      Duration(
+        days: (date.weekday - day) % DateTime.daysPerWeek,
+      ),
+    );
+  }
 }

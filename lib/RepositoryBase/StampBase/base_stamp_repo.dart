570a2366b8665d9a64c72381647b8023cdf54ec8
Dylan Baker <dylanbaker7@gmail.com>
2023-10-20 14:36:11 -0400

    a

--- lib/RepositoryBase/StampBase/base_stamp_repo.dart
+++ lib/RepositoryBase/StampBase/base_stamp_repo.dart
@@ -1,4 +1,5 @@
 import 'package:stripes_backend_helper/RepositoryBase/AuthBase/auth_user.dart';
+import 'package:stripes_backend_helper/RepositoryBase/QuestionBase/question_repo_base.dart';
 import 'package:stripes_backend_helper/RepositoryBase/SubBase/sub_user.dart';
 
 import 'stamp.dart';
@@ -6,8 +7,13 @@ import 'stamp.dart';
 abstract class StampRepo<T extends Stamp> {
   final AuthUser authUser;
   final SubUser currentUser;
+  final QuestionRepo questionRepo;
   DateTime? earliest;
-  StampRepo({required this.authUser, required this.currentUser, this.earliest});
+  StampRepo(
+      {required this.authUser,
+      required this.currentUser,
+      required this.questionRepo,
+      this.earliest});
   Stream<List<T>> get stamps;
   addStamp(T stamp);
   removeStamp(T stamp);

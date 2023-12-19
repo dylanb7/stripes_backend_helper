c246b8ae165452aa388d63c4de3bfb2c1a77fbb2
Dylan Baker <dylanbaker7@gmail.com>
2023-12-19 01:48:51 -0500

    d

--- lib/RepositoryBase/TestBase/base_test_repo.dart
+++ lib/RepositoryBase/TestBase/base_test_repo.dart
@@ -103,4 +103,5 @@ abstract class Test<T extends TestObj> {
   Future<void> submit(DateTime submitTime);
   Future<void> setValue(T obj);
   Future<void> cancel();
+  Widget? displayState(T? obj, BuildContext context);
 }
